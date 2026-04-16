076c924fbd73786eeee320d34aba7320b0fc4277
Ian Gurgel <iangurgel25@hotmail.com>
2026-04-16 17:24:57 -0300

    (backendphp): feito classe com função de conectar com o db e retornar o pdo. Dado que o projeto rodará localmente, não adicionarei arquivos env ocultos

--- backendphp/src/Database/Connection.php
+++ backendphp/src/Database/Connection.php
@@ -1,1 +1,20 @@
 <?php
+
+class Connection {
+    public static function get(): PDO {
+        try {
+            $pdo = new PDO(
+                "mysql:host=localhost;dbname=apiLIS-schema;charset=utf8mb4",
+                "root",
+                "password"
+            );
+
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+            return $pdo;
+        } catch (PDOException $e) {
+            die("Erro ao conectar no banco: " . $e->getMessage());
+        }
+    }
+}
