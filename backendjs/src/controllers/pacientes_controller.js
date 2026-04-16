d9cf5cf4422405c53410ef6e27d0f75cd6d539b6
Ian Gurgel <iangurgel25@hotmail.com>
2026-04-16 20:50:19 -0300

    (backendjs): adicionado metodo put updatePacienteById

--- backendjs/src/controllers/pacientes_controller.js
+++ backendjs/src/controllers/pacientes_controller.js
@@ -14,7 +14,7 @@ export async function getPacientes(req, res) {
 export async function getPacienteById(req, res) {
     try {
         const { id } = req.params;
-        const [row] = await db.query("SELECT * FROM tb_pacientes WHERE id = ?", id);
+        const [row] = await db.query("SELECT * FROM tb_pacientes WHERE id = ?", [id]);
         return res.json(row);
     } catch (error) {
         console.error(error);
@@ -54,6 +54,40 @@ export async function registerPaciente(req, res) {
         })
     }
 
+}
+
+export async function updatePacienteById(req, res) {
+    try {
+        const { id } = req.params;
+        const { nome, dataNascimento, carteirinha, cpf } = req.body;
+
+        if (!nome || !dataNascimento || !carteirinha || !cpf) {
+            return res.status(400).json({ message: "Todos os campos são necessários para a atualização." });
+        }
+
+        const dataFormatada = dataNascimento.split('T')[0];
+
+        const sql = `
+            UPDATE tb_pacientes 
+            SET nome = ?, dataNascimento = ?, carteirinha = ?, cpf = ? 
+            WHERE id = ?
+        `;
+
+        const [result] = await db.query(sql, [nome, dataFormatada, carteirinha, cpf, id]);
+
+        if (result.affectedRows === 0) {
+            return res.status(404).json({ message: "Paciente não encontrado para atualização." });
+        }
+
+        return res.json({
+            message: "Paciente atualizado com sucesso.",
+            data: { id, nome, dataNascimento, carteirinha, cpf }
+        });
+
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: "Erro ao atualizar paciente." });
+    }
 }
 
 export async function deletePacienteById(req, res) {
--- backendjs/src/routes/pacientes_routes.js
+++ backendjs/src/routes/pacientes_routes.js
@@ -1,12 +1,15 @@
 import { Router } from "express";
 
-import { deletePacienteById, getPacienteById, getPacientes, registerPaciente } from "../controllers/pacientes_controller.js";
+import { deletePacienteById, getPacienteById, getPacientes, registerPaciente, updatePacienteById } from "../controllers/pacientes_controller.js";
 
 const router = Router();
 
 router.get("/api/v1/pacientes", getPacientes);
 router.get("/api/v1/pacientes/:id", getPacienteById);
 router.post("/api/v1/pacientes", registerPaciente);
+
+router.put("/api/v1/pacientes/:id", updatePacienteById);
+
 router.delete("/api/v1/pacientes/:id", deletePacienteById);
 
 export default router;
