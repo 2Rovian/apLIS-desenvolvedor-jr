fb5e332387442445c51ec1185b49aa2c1898b67d
Ian Gurgel <iangurgel25@hotmail.com>
2026-04-16 23:23:52 -0300

    (app, backendjs): feito base da PacientesTableComp, import de cors no index.js

--- backendjs/src/index.js
+++ backendjs/src/index.js
@@ -1,8 +1,15 @@
 import express from "express";
 import pacientes_routes from "./routes/pacientes_routes.js";
+import cors from "cors";
 
 const app = express();
 const port = 3000;
+
+app.use(cors({
+  origin: "http://localhost:5173",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type"]
+}));
 
 app.use(express.json());
 app.use(pacientes_routes);
